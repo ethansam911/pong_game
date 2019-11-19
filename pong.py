d423e111d987a56cc894593cd281cc1e10844b6e
Ethan <esam5556@gmail.com>
2019-11-19 18:45:57 -0500

    added paddles

--- pong.py
+++ pong.py
@@ -1,31 +1,31 @@
-#Pong game 
+#Pong game
 #turtle module getting started with games
 import turtle
 import os
 
 #Screen capitalized
-win = turtle.Screen() 
+win = turtle.Screen()
 win.title("Pong by Ethan")
 
 win.bgcolor("black")
 
-win.setup(width = 800, height = 600)
-#Stops window from updating 
+win.setup(width=800, height=600)
+#Stops window from updating
 win.tracer(0)
 
 #0 0 is at the center
-#-300 down, -400 left, +400 right, +300 up 
+#-300 down, -400 left, +400 right, +300 up
 
 #Main game loop
 #Can't mix spaces and tabs
-#This doesnt use OOP because its for beginners 
+#This doesnt use OOP because its for beginners
 #And this is my refresher
 #Pygame is good for full-fledged games but turtles are better for beginners
 
 
 # Paddle A
 #Capital T for class name
-#lowercase t for module turtle 
+#lowercase t for module turtle
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -35,7 +35,7 @@ paddle_a.penup()
 paddle_a.goto(-300, 0)
 
 
-# Paddle B 
+# Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -52,14 +52,30 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 #X Y coordinates
-ball.goto(0, 0)   
-  
+ball.goto(0, 0)
 
 
+#Function - One piece at a time
+def paddle_a_up():
+    y = paddle_a.ycor()
+    #Add 20 pixels
+    y += 20
+    paddle_a.sety(y)
 
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    #Subtract 20 pixels
+    y -= 20
+    paddle_a.sety(y)
+
+
+# Keyboard  bindings
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
 
 #Old school programming methodology
+#Main game while loop
 while True:
-    win.update()    
-    
-
+    win.update()
